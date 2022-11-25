d1550e89d7bffc54b7a12bc8fcf671845ef230ab
danovise <novik301199@gmail.com>
2022-11-25 19:12:08 +0300

    build(app): adding UICollectionViewCell for MainVC

---
+++ quiz-project/Screens/Main/Cells/CategoryCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionCell.swift
+//  quiz-project
+//
+//  Created by Daria Sechko on 25.11.22.
+//
+
+import UIKit
+
+class CategoryCollectionCell: UICollectionViewCell {
+    
+}
---
+++ quiz-project/Screens/Main/Cells/GameModeCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameModeCollectionCell.swift
+//  quiz-project
+//
+//  Created by Daria Sechko on 25.11.22.
+//
+
+import UIKit
+
+class GameModeCollectionCell: UICollectionViewCell {
+    
+}
