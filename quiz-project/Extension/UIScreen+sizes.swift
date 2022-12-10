ab224697236881eb2fd1ac1633142560fddf72b3
danovise <novik301199@gmail.com>
2022-12-10 19:53:52 +0300

    build(app): adding category model

---
+++ quiz-project/Extension/UIScreen+sizes.swift
@@ -0,0 +1,8 @@
+//
+//  UIScreen+sizes.swift
+//  quiz-project
+//
+//  Created by Daria Sechko on 10.12.22.
+//
+
+import Foundation
---
+++ quiz-project/Models/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  quiz-project
+//
+//  Created by Daria Sechko on 10.12.22.
+//
+
+import Foundation
+
+class Category {
+
+    var name: String
+    var selected = false
+    
+    init(name: String) {
+        self.name = name
+    }
+}
