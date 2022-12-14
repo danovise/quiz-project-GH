78079b9bf46392e01cfd366d36cf0f91b47864b7
danovise <novik301199@gmail.com>
2022-12-14 12:57:38 +0300

    build(auth): changed the authorisation screen

--- quiz-project/Screens/Auth/AuthVC.swift
+++ quiz-project/Screens/Auth/AuthVC.swift
@@ -8,27 +8,16 @@
 import UIKit
 import FirebaseAuth
 
+enum AuthType: Int {
+    case login
+    case registration
+}
+
 class AuthVC: UIViewController {
     
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.backgroundColor = .black
-        scrollView.bounces = true
-        
-        return scrollView
-    }()
+    var authType: AuthType? = .login
     
-    private let stackViewContainer: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        
-        return stackView
-    }()
-    
-    private var imageView: UIImageView = {
+    private let imageView: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.image = UIImage(named: "main")
@@ -37,179 +26,197 @@ class AuthVC: UIViewController {
         return image
     }()
     
-    private var signLabel: UILabel = {
+    private let loginLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.boldSystemFont(ofSize: 40)
-        label.text = "Sign up"
+        label.text = "Авторизация"
         label.textAlignment = .center
         label.textColor = .white
-        label.numberOfLines = 0
-        label.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
+        label.font = UIFont.boldSystemFont(ofSize: 40)
         return label
     }()
-    
-    private var emailTextField: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.contentInsetAdjustmentBehavior = .automatic
-        textView.backgroundColor = .white
-        textView.text = "Email"
-        textView.isScrollEnabled = false
-        textView.textColor = .lightGray
-        textView.font = .systemFont(ofSize: 20)
-        textView.layer.cornerRadius = 10
-        textView.textContainerInset = .init(top: 20, left: 20, bottom: 20, right: 20)
         
-        return textView
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Email"
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.backgroundColor = .white
+        textField.font = .systemFont(ofSize: 20)
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        return textField
     }()
     
-    private let usernameTextField: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.contentInsetAdjustmentBehavior = .automatic
-        textView.backgroundColor = .white
-        textView.text = "Name"
-        textView.isScrollEnabled = false
-        textView.textColor = .lightGray
-        textView.font = .systemFont(ofSize: 20)
-        textView.layer.cornerRadius = 10
-        textView.textContainerInset = .init(top: 20, left: 20, bottom: 20, right: 20)
-        
-        return textView
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Password"
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .white
+        textField.font = .systemFont(ofSize: 20)
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        return textField
     }()
-    
-    private let passwordTextField: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.contentInsetAdjustmentBehavior = .automatic
-        textView.backgroundColor = .white
-        textView.text = "Password"
-        textView.isScrollEnabled = false
-        textView.textColor = .lightGray
-        textView.font = .systemFont(ofSize: 20)
-        textView.layer.cornerRadius = 10
-        textView.textContainerInset = .init(top: 20, left: 20, bottom: 20, right: 20)
-        
-        return textView
-    }()
-    
-    private let answerButton: UIButton = {
-        let button = UIButton()
+ 
+    private let entryButton: UIButton = {
+        let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Create account", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Войти", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.backgroundColor = #colorLiteral(red: 0.01418284792, green: 0.5850355029, blue: 0.9913098216, alpha: 1)
+        button.layer.borderWidth = 1
         button.layer.cornerRadius = 10
-        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
-        button.addTarget(self, action: #selector(registrationToFirebase), for: .touchUpInside)
+        button.addTarget(self, action: #selector(entryButtonTapped), for: .touchUpInside)
         
         return button
     }()
     
-    private let signInButton: UIButton = {
-        let button = UIButton()
+    private let entryTypeButton: UIButton = {
+        let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Already have an account? Sign in", for: .normal)
-        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
-        button.addTarget(self, action: #selector(showSignInScreen), for: .touchUpInside)
-        
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Зарегистроваться", for: .normal)
+        button.addTarget(self, action: #selector(entryTypeButtonTapped), for: .touchUpInside)
         return button
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupViews()
         setupConstraints()
-        
-        showMainScreen()
     }
     
-    func setupViews(){
-        
-        view.addSubview(scrollView)
-        scrollView.addSubview(stackViewContainer)
-        
-        stackViewContainer.addArrangedSubview(imageView)
-        stackViewContainer.addArrangedSubview(signLabel)
-        stackViewContainer.addArrangedSubview(emailTextField)
-        stackViewContainer.addArrangedSubview(usernameTextField)
-        stackViewContainer.addArrangedSubview(passwordTextField)
-        stackViewContainer.addArrangedSubview(answerButton)
-        
-        stackViewContainer.addArrangedSubview(signInButton)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
-    func setupConstraints(){
-        NSLayoutConstraint.activate([
-            
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            
-            stackViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 12),
-            stackViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10),
-            stackViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -10),
-            stackViewContainer.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 20)
-        ])
+    // MARK: - Private
+    
+    private func setupViews() {
+        view.backgroundColor = .black
+        view.addSubview(imageView)
+        view.addSubview(loginLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(entryButton)
+        view.addSubview(entryTypeButton)
     }
     
-    //MARK: - Navigation
+    private func setupConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top).inset(50)
+            $0.centerX.equalTo(view.snp_centerXWithinMargins)
+        }
+        
+        loginLabel.snp.makeConstraints {
+            $0.top.equalTo(view.snp_topMargin).inset(160)
+            $0.centerX.equalTo(view.snp_centerXWithinMargins)
+        }
+        
+        emailTextField.snp.makeConstraints {
+            $0.top.equalTo(loginLabel).inset(100)
+            $0.left.right.equalToSuperview().inset(50)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(emailTextField).inset(60)
+            $0.left.right.equalToSuperview().inset(50)
+        }
+        
+        entryButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField).inset(65)
+            $0.left.right.equalToSuperview().inset(50)
+        }
+        
+        entryTypeButton.snp.makeConstraints {
+            $0.top.equalTo(entryButton).inset(80)
+            $0.left.right.equalToSuperview().inset(50)
+        }
+    }
+    
+    @objc
+    private func entryButtonTapped() {
+        switch authType {
+        case .login:
+            authorizeToFirebase()
+        case .registration:
+            registrationToFirebase()
+        case .none:
+            return
+        }
+    }
+    
+    @objc
+    private func entryTypeButtonTapped() {
+        switch authType {
+        case .login:
+            loginLabel.text = "Регистрация"
+            entryButton.setTitle("Зарегистрироваться", for: .normal)
+            entryTypeButton.setTitle("Уже есть аккаунт", for: .normal)
+            authType = .registration
+        case .registration:
+            loginLabel.text = "Авторизация"
+            entryButton.setTitle("Войти", for: .normal)
+            entryTypeButton.setTitle("Зарегистрироваться", for: .normal)
+            authType = .login
+        case .none:
+            return
+        }
+    }
+    
+    // MARK: - Navigation
+    
     func showMainScreen() {
-        
-        let mainVC = ScreenFactoryImpl().makeMainScreen()
-        self.navigationController?.pushViewController(mainVC, animated: true)
+        let mainViewController = ScreenFactoryImpl().makeMainScreen()
+        self.navigationController?.pushViewController(mainViewController, animated: true)
     }
     
-//        //Логин - вход в аккаунт
-//        func authorizeToFirebase() {
-//
-//            Auth.auth().signIn(withEmail: "test@test.com", password: "test12345678") { result, error in
-//
-//                if let error = error {
-//
-//                    print(error.localizedDescription)
-//                    return
-//                }
-//
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//                    self.showMainScreen()
-//                }
-//            }
-//        }
-    
-    //Создание аккаунта
-    @objc func registrationToFirebase() {
-        
-        Auth.auth().createUser(withEmail: "testtest@test.com", password: "12345678") { result, error in
-            
+    func authorizeToFirebase() {
+        Auth.auth().signIn(withEmail: "\(emailTextField.text ?? "")", password: "\(emailTextField.text ?? "")") { result, error in
             if let error = error {
-                
                 print(error.localizedDescription)
+                let alertController = UIAlertController(title: "", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Ok", style: .cancel)
+                alertController.addAction(alertAction)
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.showMainScreen()
+            }
+        }
+    }
+    
+    func registrationToFirebase() {
+        Auth.auth().createUser(withEmail: "\(emailTextField.text ?? "")", password: "\(emailTextField.text ?? "")") { result, error in
+            if result != nil {
+                let alertController = UIAlertController(title: "", message: "Registration successful", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Ok", style: .cancel)
+                alertController.addAction(alertAction)
+                self.present(alertController, animated: true, completion: {
+                    self.entryTypeButtonTapped()
+                })
+            }
+            if let error = error {
+                print(error.localizedDescription)
+                let alertController = UIAlertController(title: "", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Ok", style: .cancel)
+                alertController.addAction(alertAction)
+                self.present(alertController, animated: true, completion: nil)
                 return
             }
         }
     }
     
-    //Логаут - выход из аккаунта
     func logoutFromFirebase() {
-        
         do {
             try Auth.auth().signOut()
-            
         } catch {
             print(error.localizedDescription)
         }
-    }
-    
-    //MARK: - Action
-    @objc func showSignInScreen() {
-        let signInVC = ScreenFactoryImpl().makeSignInVC()
-        self.navigationController?.pushViewController(signInVC, animated: true)
     }
 }
